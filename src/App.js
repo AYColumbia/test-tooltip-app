b9d7535c2e70247393338ad58ae48a00bef5fd3d
Arthur Yousif <arthur.yousif@gmail.com>
2024-07-01 21:41:32 -0500

    attempt to duplicate react-tooltip warnings when using a 2nd component that also uses the tooltip

--- src/App.js
+++ src/App.js
@@ -1,78 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import jdata from './data.json';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import AsyncSelect from 'react-select/async';
-import { Formik } from 'formik';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import React, { Fragment, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Redirect, Routes, Route, Navigate } from 'react-router-dom';
+import TestComponent from './components/TestComponent';
+import TestComponent2 from './components/TestComponent2';
 import 'react-data-grid/lib/styles.css';
-import DataGrid from 'react-data-grid';
-import { Printer } from 'react-bootstrap-icons'
 import './App.css';
-import { Tooltip } from 'react-tooltip';
-import debounce from 'lodash.debounce';
 
 function App() {
-  const [listData, setListData] = useState([]);
-  const loadOptions = debounce((query, cbfn) => {
-    cbfn(listData.filter((i) => i.label.toLowerCase().indexOf(query.toLowerCase()) >= 0));
-  }, 500);
-
+  const [crBlurb, setCrBlurb] = useState(<Fragment>Copyright &copy; 2024 AYColumbia</Fragment>)
   useEffect(() => {
-    let data = [];
-    Array.apply(null, Array(10)).forEach((a, i) => data.push({ value: i, label: `Item ${Math.random() * (25 - 1) + 1}` }));
-    setListData(data);
   }, []);
-  useEffect(() => {
-  }, [listData])
-
-  const columns = [
-    { key: 'name', name: 'Name' },
-    { key: 'abbreviation', name: 'Abbreviation' },
-    { key: 'capital', name: 'Capital' },
-    { key: 'timezone', name: 'Time Zone' }
-  ];
-  const rows = jdata.states;
 
   return (
+    <>
     <div>
-      <Formik>
-        {(formikProps) =>
-        (
-          <>
-            <Form className='form-control p-4'>
-              <Row>
-                <Form.Group as={Col} md={8}>
-                  <Form.Label>This is a label</Form.Label>
-                </Form.Group>
-              </Row>
-              <Row>
-                <Form.Group as={Col} md={4}>
-                  <div data-tooltip-id='custom-tooltip' data-tooltip-content={'Type to filter items'}>
-                    <AsyncSelect cacheOptions
-                      defaultOptions={listData}
-                      loadOptions={loadOptions} />
-                  </div>
-                </Form.Group>
-              </Row>
-              <Row className='pt-2'>
-                <Form.Group as={Col} md={8}>
-                  <DataGrid columns={columns} rows={rows} />
-                </Form.Group>
-              </Row>
-              <Row className='pt-2'>
-                <Form.Group as={Col} md={4}>
-                <Button type='submit'><Printer/> Click Me</Button>
-                </Form.Group>
-              </Row>
-            </Form>
-            <Tooltip id="custom-tooltip" place="right" delayHide={2000} />
-          </>
-        )}
-      </Formik>
+      <Router>
+        <Routes>
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<TestComponent />} />
+          <Route path="/T2" element={<TestComponent2 />} />
+        </Routes>
+      </Router>
     </div>
+    <footer>{crBlurb}</footer>
+    </>
   );
 }
 
---
+++ src/components/CustomTooltip.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Tooltip } from 'react-tooltip';
+
+const CustomTooltip = (props) =>
+{
+
+    return (
+        <>
+            <Tooltip {...props} />
+        </>
+    );
+};
+
+export default CustomTooltip;
